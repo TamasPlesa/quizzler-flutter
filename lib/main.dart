abe815000fd271ada17dc8403e0e53fda14376a1
TamasPlesa <tamasplesa@icloud.com>
2021-02-22 00:52:28 +0100

    added questions, button click works, created question object

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,22 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +77,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  bool correctAnswer = answers[questionNumber];
+
+                  if (correctAnswer == true) {
+                    print('User got it right');
+                  } else {
+                    print('User got it wrong');
+                  }
+
+                  questionNumber++;
+                });
               },
             ),
           ),
@@ -79,11 +105,24 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  bool correctAnswer = answers[questionNumber];
+
+                  if (correctAnswer == true) {
+                    print('User got it right');
+                  } else {
+                    print('User got it wrong');
+                  }
+
+                  questionNumber++;
+                });
               },
             ),
           ),
         ),
+        Row(
+          children: scoreKeeper,
+        )
         //TODO: Add a Row here as your score keeper
       ],
     );
